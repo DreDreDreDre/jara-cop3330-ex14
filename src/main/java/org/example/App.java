86880dfcdd9d79005899fa339b9bacf8e9a34870
DreDreDreDre <andrejar34@gmail.com>
2021-09-10 17:58:35 -0400

    Exercise 14

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,10 @@
 package org.example;
 import java.util.Scanner;
-/**
- * Hello world!
- *
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Andre Jara
  */
+
 public class App 
 {
     public static void main(String[] args)
